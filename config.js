9a5a6d07c06697d1edfa314b9e3f91c117731741
Hnhann65 <86118564+Hnhann65@users.noreply.github.com>
2021-06-18 22:19:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,9 +11,9 @@ const CONFIG = {
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    reply: '<3',
+    mess: ' Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Gửi những lời yêu thương cho nhau nào .',
+    btnAccept: 'Oki',
+    messLink: 'https://m.me/Hnhann.0310' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
